08c0e2ce5b0be4e464e5c72daa7228eeb6b62624
hasan-creatoros <146756852+hasan-creatoros@users.noreply.github.com>
2023-10-21 20:27:41 +0300

    clean

--- web-app/src/pages/create-credential.tsx
+++ web-app/src/pages/create-credential.tsx
@@ -1,19 +1,11 @@
 'use client'
-import { useState } from 'react'
 import { Connected } from '../components/Connected'
 import Navbar from '../components/Navbar'
-import ProviderSelectInput from '../components/ProviderSelectInput'
-import { providerType } from '../utils/types'
-import { Center, Container, Flex, Stack } from '@chakra-ui/layout'
+import { Center, Flex, Stack } from '@chakra-ui/layout'
 import { CustomStepper } from '../components/CustomStepper'
 
 export default function Page () {
-  const [selectedProviders, setSelectedProviders] = useState<providerType[]>([])
-
-  const handleSelectChange = (selected: any) => {
-    setSelectedProviders(selected)
-  }
-
+  
   return (
     <>
       <Navbar showConnectWalletButton />
